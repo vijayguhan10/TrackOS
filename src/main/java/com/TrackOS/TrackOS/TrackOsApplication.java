cf07f243e8af423639579a8561b170b8ebff3373
vijayguhan10 <vijayguhan10@gmail.com>
2026-01-08 23:29:46 +0530

    created a professional Structure of the Application

--- src/main/java/com/TrackOS/TrackOS/TrackOsApplication.java
+++ src/main/java/com/TrackOS/TrackOS/TrackOsApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrackOsApplication {
-
 	public static void main(String[] args) {
+		System.out.println("Starting TrackOS Application...");
 		SpringApplication.run(TrackOsApplication.class, args);
 	}
 

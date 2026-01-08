57ae6eb7104402fbce0db73d2166c83d08108fc8
vijayguhan10 <vijayguhan10@gmail.com>
2026-01-08 23:05:20 +0530

    Intializing the project with the base configuration

---
+++ src/main/java/com/TrackOS/TrackOS/TrackOsApplication.java
@@ -0,0 +1,13 @@
+package com.TrackOS.TrackOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrackOS/TrackOS/TrackOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrackOS.TrackOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
